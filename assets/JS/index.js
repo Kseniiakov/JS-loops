b1c27221c79aec55f7cffae86fbe3acd59f09086
Kseniia Kovalenko <kseniiakovalenko.94@gmail.com>
2022-02-01 17:03:31 +0200

    Add 6 assignments

---
+++ assets/JS/index.js
@@ -0,0 +1,73 @@
+// 1.1) вывести в консоль числа от 1 до 10 включительно
+
+// for (let num = 1; num <= 10; num++) {
+//   console.log(num);
+// }
+
+// 1.2) * вывести в консоль числа от -1 до -10 включительно
+
+// for (let num = -1; num >= -10; num--) {
+//   console.log(num);
+// }
+
+// 2) посчитать результат умножения чисел от 1 до 5
+
+// for (let num = 1; num <= 5; num++) {
+//   for (let factor = 1; factor <= 5; factor++) {
+//     console.log(num,'*', factor, '=', num*factor);
+//   }
+// }
+
+// 3) вывести в консоль таблицу умножения на 7
+// пример ('7 * ' + n + ' = ' + res)
+
+// for (let num = 7; num <= 7; num++) {
+//   console.log('Умножение на', num);
+//   for (let factor = 1; factor <= 9; factor++) {
+//     console.log(num,'*', factor, '=', num*factor);
+//   }
+// }
+
+// 4) посчитать сумму нечетных чисел от 1 до 15
+
+// for (let num = 1; num <= 15; num += 2) {
+//   for (let factor = 1; factor <= 15; factor += 2) {
+//     console.log(num,'+', factor, '=', num+factor);
+//   }
+// }
+
+// 5.1) циклом добавить 7 звездочек в строку 
+
+// for (let num = '*'; num.length <=7; num += '*') {
+//   console.log(num);
+// }
+
+// 5.2) * кол-во звездочек определяет пользователь
+
+// let userNum = prompt('введите число звездочек');
+
+// for (let num = '*'; num.length <= userNum; num += '*') {
+//   console.log(num);
+// }
+
+// 6) * 2 цикла которые добавят звездочки и "\n" 
+// в переменную и составят квадрат из звездочек,
+
+let myNum = 8; 
+let result = row;
+
+// for (let num = '*'; num.length <= myNum; num += '*') {
+  // for (let num = '*'; num.length <= myNum; num += '*') {
+    // if (num = myNum) {
+      // console.log(num);
+    // }
+  // }
+// }
+
+  // for (let col = 1; col <= myNum; col++) {
+    // for (let row = '*'; row.length <= myNum; row += '*') {
+      // console.log(result);
+  //   }
+
+  //   col += '/n';
+  // }
