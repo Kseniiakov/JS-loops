94f526ed04d7231693c11c8e9bd0d4302af09f86
Kseniia Kovalenko <kseniiakovalenko.94@gmail.com>
2022-02-02 10:50:40 +0200

    Add task 7 and slight changes

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -12,15 +12,13 @@
 
 // 2) посчитать результат умножения чисел от 1 до 5
 
-let result = 1;
-
-for (let num = 1; num <= 5; num++) {
-  result *= num;
-}
-console.log(result);
+// let result = 1;
+// for (let num = 1; num <= 5; num++) {
+//   result *= num;
+// }
+// console.log(result);
 
 // 3) вывести в консоль таблицу умножения на 7
-// пример ('7 * ' + n + ' = ' + res)
 
 // for (let num = 7; num <= 7; num++) {
 //   console.log('Умножение на', num);
@@ -31,44 +29,70 @@ console.log(result);
 
 // 4) посчитать сумму нечетных чисел от 1 до 15
 
+// let result = 0;
 // for (let num = 1; num <= 15; num += 2) {
-//   for (let factor = 1; factor <= 15; factor += 2) {
-//     console.log(num,'+', factor, '=', num+factor);
-//   }
+//   result += num;
 // }
+// console.log(result);
 
 // 5.1) циклом добавить 7 звездочек в строку 
 
-// for (let num = '*'; num.length <=7; num += '*') {
-//   console.log(num);
+// let stars = ''
+// for (let num = 1; num <= 7; num ++) {
+//   stars += '*';
 // }
+// console.log(stars);
 
 // 5.2) * кол-во звездочек определяет пользователь
 
-// let userNum = prompt('введите число звездочек');
+// let userNum = Number (prompt('введите число звездочек'));
 
-// for (let num = '*'; num.length <= userNum; num += '*') {
-//   console.log(num);
+// function sandbox (userStars) {
+
+//   let stars = ''
+//   for (let num = 1; num <= userStars; num ++) {
+//     stars += '*';
+//   }
+//   return stars;
 // }
+
+// const result = sandbox (userNum);
+
+// console.log(result);
 
 // 6) * 2 цикла которые добавят звездочки и "\n" 
 // в переменную и составят квадрат из звездочек,
 
-// let myNum = 8; 
-// let result = row;
 
-// for (let num = '*'; num.length <= myNum; num += '*') {
-  // for (let num = '*'; num.length <= myNum; num += '*') {
-    // if (num = myNum) {
-      // console.log(num);
-    // }
-  // }
+// let userNum = Number (prompt('введите число звездочек'));
+
+// function sandbox (userStars) {
+
+//   let stars = ''
+//   for (let row = 1; row <= userStars; row ++) {
+//     for (let num = 1; num <= userStars; num ++) {
+//       stars += '*';
+//     }
+//     stars += '\n';
+//   }
+//   return stars;
 // }
 
-  // for (let col = 1; col <= myNum; col++) {
-    // for (let row = '*'; row.length <= myNum; row += '*') {
-      // console.log(result);
-  //   }
+// const result = sandbox (userNum);
+// console.log(result);
 
-  //   col += '/n';
-  // }
+// 7) * спрашивать пользователя определенное число 
+// до тех пор пока он его не отгадает
+
+// function guessNumber () {
+//   for ( ; ; ) {
+//     let userNum = Number (prompt('угадайте число'));
+//     if (userNum === 6) {
+//       alert ('вы угадали :)');
+//       break;
+//     }
+//     alert ('вы не угадали, попробуйте еще раз :(');
+//   }
+// }
+
+// guessNumber();
